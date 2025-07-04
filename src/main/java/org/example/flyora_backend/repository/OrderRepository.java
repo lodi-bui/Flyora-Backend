1a5445ff6c9b577e96656f8969efe5274859fd98
Huntrot <captainparrot13042005@gmail.com>
2025-07-04 07:47:16 +0700

    fixing order api

--- src/main/java/org/example/flyora_backend/repository/OrderRepository.java
+++ src/main/java/org/example/flyora_backend/repository/OrderRepository.java
@@ -1,9 +1,16 @@
 package org.example.flyora_backend.repository;
 
 import java.util.List;
+import java.util.Optional;
+
 import org.example.flyora_backend.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface OrderRepository extends JpaRepository<Order, Integer> {
     List<Order> findByCustomerIdOrderByCreatedAtDesc(Integer customerId);
+
+    @Query("SELECT MAX(o.id) FROM Order o")
+    Optional<Integer> findMaxId();
+
 }
--- src/main/java/org/example/flyora_backend/repository/PaymentRepository.java
+++ src/main/java/org/example/flyora_backend/repository/PaymentRepository.java
@@ -1,8 +1,13 @@
 package org.example.flyora_backend.repository;
 
+import java.util.Optional;
+
 import org.example.flyora_backend.model.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PaymentRepository extends JpaRepository<Payment, Integer> {
+    @Query("SELECT MAX(p.id) FROM Payment p")
+    Optional<Integer> findMaxId();
 
 }
